86cf06483af9e3a164f18f08ed8ebc984bc8394e
deep04shikha11 <deepshikhaverma44@gmail.com>
2026-04-30 16:04:00 +0530

    Flutter Use BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class FlutterApp extends StatelessWidget {
+  const FlutterApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,18 +18,31 @@ class FlutterApp extends StatelessWidget {
 }
 
 class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Dashboard')),
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         color: Colors.blue.shade100,
-        child: const Center(
-          child: Text(
-            'Welcome to the Dashboard',
-            style: TextStyle(
-              fontSize: 24,
-              color: Color.fromARGB(255, 177, 75, 75),
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              // borderRadius: BorderRadius.circular(11),
+              border: Border.all(width: 3, color: Colors.yellow.shade300),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15,
+                  color: Colors.red.shade400,
+                  spreadRadius: 1,
+                ),
+              ],
+              shape: BoxShape.circle,
             ),
           ),
         ),
