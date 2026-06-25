9156cf846b2d1e67baef795cbd655e7b7b503c01
deep04shikha11 <deepshikhaverma44@gmail.com>
2026-06-25 16:36:20 +0530

    applying Flutter Styles and Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hot_load_app/ui_helper/utility.dart';
 
 void main() {
   runApp(FlutterApp());
@@ -11,7 +12,15 @@ class FlutterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          titleMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          bodyLarge: TextStyle(fontSize: 16),
+          bodyMedium: TextStyle(fontSize: 14),
+        ),
+      ),
       home: DashboardScreen(),
     );
   }
@@ -21,68 +30,99 @@ class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    var arrNames = [
-      'John',
-      'Jane',
-      'Doe',
-      'Smith',
-      'Alice',
-      'Bob',
-      'Charlie',
-      'David',
-      'Eve',
-      'Frank',
-    ];
+    // var arrNames = [
+    //   'John',
+    //   'Jane',
+    //   'Doe',
+    //   'Smith',
+    //   'Alice',
+    //   'Bob',
+    //   'Charlie',
+    //   'David',
+    //   'Eve',
+    //   'Frank',
+    // ];
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: ListView.separated(
-        itemBuilder: (context, index) {
-          return ListTile(
-            // leading: CircleAvatar(child: Text('${index + 1}')),
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/profile.png'),
-              backgroundColor: Colors.lightGreen.shade300,
-              maxRadius: 30,
-              child: Text(
-                arrNames[index][0],
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Nunito',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            title: Text(arrNames[index]),
-            subtitle: const Text('Designation'),
-            trailing: const Icon(Icons.arrow_forward_ios),
-          );
-        },
-        separatorBuilder: (context, index) =>
-            const Divider(height: 10, thickness: 2, color: Colors.grey),
-        itemCount: 20,
+      appBar: AppBar(
+        backgroundColor: Colors.orange.shade300,
+        title: const Text('Dashboard'),
       ),
-      // Column(
-      //   children: [
-      //     Expanded(
-      //       flex: 2,
-      //       child: Container(height: 100, color: Colors.blue.shade300),
-      //     ),
-      //     Expanded(
-      //       flex: 2,
-      //       child: Container(height: 100, color: Colors.yellow.shade300),
-      //     ),
-      //     Expanded(
-      //       flex: 2,
-      //       child: Container(height: 100, color: Colors.red.shade300),
-      //     ),
-      //     Expanded(
-      //       flex: 2,
-      //       child: Container(height: 100, color: Colors.green.shade300),
-      //     ),
-      //   ],
-      // ),
+      body: Column(
+        children: [
+          Text(
+            'Hello one',
+            style: Theme.of(
+              context,
+            ).textTheme.displayLarge!.copyWith(color: Colors.blue),
+          ),
+          Text(
+            'Hello two',
+            style: myTextStyle(color: Colors.lightBlue.shade300),
+          ),
+          Text(
+            'Hello three',
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge!.copyWith(color: Colors.red),
+          ),
+          Text(
+            'Hello four',
+            style: myTextStyle(
+              fontSize: 11,
+              fontWeight: FontWeight.bold,
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+      //    ListView.separated(
+      //     itemBuilder: (context, index) {
+      //       return ListTile(
+      //         // leading: CircleAvatar(child: Text('${index + 1}')),
+      //         leading: CircleAvatar(
+      //           backgroundImage: AssetImage('assets/images/profile.png'),
+      //           backgroundColor: Colors.lightGreen.shade300,
+      //           maxRadius: 30,
+      //           child: Text(
+      //             arrNames[index][0],
+      //             style: const TextStyle(
+      //               color: Colors.white,
+      //               fontFamily: 'Nunito',
+      //               fontSize: 20,
+      //               fontWeight: FontWeight.bold,
+      //             ),
+      //           ),
+      //         ),
+      //         title: Text(arrNames[index]),
+      //         subtitle: Text('Designation ${index.toString()}'),
+      //         trailing: const Icon(Icons.arrow_forward_ios),
+      //       );
+      //     },
+      //     separatorBuilder: (context, index) =>
+      //         const Divider(height: 10, thickness: 2, color: Colors.grey),
+      //     itemCount: 20,
+      //   ),
+      //   // Column(
+      //   //   children: [
+      //   //     Expanded(
+      //   //       flex: 2,
+      //   //       child: Container(height: 100, color: Colors.blue.shade300),
+      //   //     ),
+      //   //     Expanded(
+      //   //       flex: 2,
+      //   //       child: Container(height: 100, color: Colors.yellow.shade300),
+      //   //     ),
+      //   //     Expanded(
+      //   //       flex: 2,
+      //   //       child: Container(height: 100, color: Colors.red.shade300),
+      //   //     ),
+      //   //     Expanded(
+      //   //       flex: 2,
+      //   //       child: Container(height: 100, color: Colors.green.shade300),
+      //   //     ),
+      //   //   ],
+      //   // ),
     );
   }
 }
---
+++ lib/ui_helper/utility.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle myTextStyle({
+  double fontSize = 14,
+  FontWeight fontWeight = FontWeight.normal,
+  Color color = Colors.black,
+}) {
+  return TextStyle(fontSize: fontSize, fontWeight: fontWeight, color: color);
+}
